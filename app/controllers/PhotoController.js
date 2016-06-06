2e34f25b18c6fc86f3d28366c53b8b393b701d73
Zakhar <blashirk@gmail.com>
2016-06-06 11:34:35 +0300

    commit 009

--- app/controllers/PhotoController.js
+++
@@ -1,5 +0,0 @@
-app.controller('PhotoController', ['$scope', 'displays', '$routeParams', function($scope, displays, $routeParams) {
-  displays.success(function(data) {
-    $scope.detail = data[$routeParams.id];
-  });
-}]);
--- app/services/getModels.js
+++
@@ -1,9 +0,0 @@
-CalcApp.factory('displays', ['$http', function($http) {
-  return $http.get('http://ekta.com.ua/projects/calc/data/getModels.json')
-         .success(function(data) {
-           return data;
-         })
-         .error(function(data) {
-           return data;
-         });
-}]);
