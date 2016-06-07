520987e03269d5269d6803dcb464e3267374a6db
Zakhar <blashirk@gmail.com>
2016-06-07 09:19:19 +0300

    commit 010

--- app/services/displays.js
+++ app/services/displays.js
@@ -9,13 +9,3 @@ CalcApp.service('Displays', ['$http', '$q', function($http, $q) {
         }
     }
 }]);
-
-// CalcApp.factory('displays', ['$http', function($http) {
-//   return $http.get('http://ekta.com.ua/projects/calc/data/getModels.json')
-//          .success(function(data) {
-//            return data;
-//          })
-//          .error(function(data) {
-//            return data;
-//          });
-// }]); 
