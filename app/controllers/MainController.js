00b54e1f76b5aee1b047f3df048e7ad39f055d52
Zakhar <blashirk@gmail.com>
2016-07-20 17:55:12 +0300

    Preview inputs filtering in progress....

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -44,13 +44,17 @@
         };
 
         vm.getDimensions = function() {
+            vm.filterInput();
             return vm.calc_data.vertical_dimension = (vm.calc_data.slider_vertical * 0.3864).toFixed(2),
                    vm.calc_data.horizontal_dimension = (vm.calc_data.slider_horizontal * 0.3864).toFixed(2);
         }
 
         vm.filterInput = function() {
-          var inputs = new RegExp(/[.]/);
-          return inputs.replace([,]);
+          var dot = new RegExp(/[.]/);
+          var coma = new RegExp(/[,]/);
+
+          return dot.replace(coma, dot);
+
         }
 
         vm.getDiagonal = function() {
